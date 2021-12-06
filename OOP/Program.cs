f6665cbe150143dfa0bd86d29e90a8f573633141
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2021-12-06 13:40:09 +0100

    Add project files.

---
+++ OOP/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            Person person = new Person();
+            person = employee;
+            Person admin = new Admin();
+            Admin admin1 = (Admin)admin;
+           
+        }
+    }
+}
