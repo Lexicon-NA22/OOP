fdc084591010702c94c03234210a7b36b5ff208a
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2021-12-07 15:43:46 +0100

    Kommentarer

--- OOP/Extensions.cs
+++ OOP/Extensions.cs
@@ -6,13 +6,20 @@ using System.Threading.Tasks;
 
 namespace OOP
 {
+    //Extension metoder låter en lägga till metoder på existerande klasser
+    //Anropas på samma sätt som vanliga instans metoder
+    //Måste ligga i en statisk klass
+    //Måste vara en statisk metod
+    //this string visar att vi "extendar" typen string
     public static class Extensions
     {
+        //Här extendar typen string med en AddString metod
         public static string AddString(this string first, string second)
         {
             return $"{first} {second}";
         }
 
+        //Här extendar vi en lista av IDrivable
         public static void Print(this List<IDrivable> source)
         {
            // source.ForEach(v => Console.WriteLine(v.Drive(34)));
--- OOP/Vehicle.cs
+++ OOP/Vehicle.cs
@@ -6,25 +6,44 @@ using System.Threading.Tasks;
 
 namespace OOP
 {
+    //Interface
+    //En klass kan implementera (ärva) från många interface
+    //Allt är publikt 
+    //(Med C# 8 kan vi har privata statiska medlemmar, samt publika/privata metoder med default implemenation)
+    //Måste Implementeras i ärvda klasser
+    //(Kan ha implementation from c# 8)
+    //Kan inte implementeras - ej skapa objekt av med new
     public interface IDrivable
     {
         string Drive(int distance);
     }
 
+    //Abstrakt
+    //Kan inte implementeras - ej skapa objekt av med new
+    //Kan inehålla en blandnig av vanliga metoder och abstrakta metoder utan implementation
+    //Alla abstrakta medlemmar måste implemneteras av dem som ärver från den abstrakta klassen
+    //Kan hålla privata fält
+    //AbstractVehicle implementerar IDrivable
     public abstract class AbstractVehicle : IDrivable
     {
+        //Virtual - En metod som markeras med nykelordet virtual är ok att skriva en ny implementation i  ärvda klasser
         public virtual string Drive(int distance)
         {
             return $"{this.GetType().Name} wants to drive for {distance}";
         }
 
+        //Håller ingen implementation måste implementeras i ärvda klasser
+        //Nykelordet abstract kan bara användas i abstrakta klasser
         public abstract string Turn();
 
     }
 
+    //Vehicle ärver från AbstractVehicle
     public class Vehicle : AbstractVehicle
     {
         public string Brand { get; set; }
+
+        //Protected enbart synlig här och klasser som ärver Vehicle. Private för alla andra
         protected string RegNo { get; set; }
 
         public Vehicle(string brand, string regNo)
@@ -33,21 +52,19 @@ namespace OOP
             RegNo = regNo;
         }
 
+        //Overide egen implementation av Turn, mer specialiserad variant
         public override string Turn()
         {
             return "Vehicle turns";
-        }
-
-        public void Test()
-        {
-
         }
     }
 
     public class FuelVehicle : Vehicle
     {
+        //Privat fält
         private double fuelLevel;
 
+        //Property, när vi skriver kod i get och set behöver vi skriva ut det bakomliggande fältet
         public double FuelLevel 
         {
             get
@@ -61,38 +78,41 @@ namespace OOP
             }
         }
 
+        //Property med enbart geter. Kan bara sättas via konstruktorn
         public double FuelCapacity { get; }
+
+        //Nyckelordet base anropar här basklassens konstruktor
         public FuelVehicle(string regNo, double fuelCapacity, string brand = "default") : base(brand, regNo)
         {
             FuelCapacity = fuelCapacity;
         }
 
-        public new void  Test()
-        {
-
-        }
-
-        public  override string Drive(int distance)
-        {
-            return base.Drive(distance) + " From FuelVehicle";
-        }
+        //public  override string Drive(int distance)
+        //{
+        //    return base.Drive(distance) + " From FuelVehicle";
+        //}
     }
 
 
     public class FuelCar : FuelVehicle
     {
+        //Const kan inte ändras när värdet är satt. Måste sätta värdet direkt!
         private const double fuelConsumption = 0.5;
 
         protected double MaxDistance => FuelLevel / fuelConsumption;
+        //Samma som ovan!
         //{
         //    get
         //    {
         //        return FuelLevel / fuelConsumption;
         //    }
         //}
+
+        //Kedjade konstruktorer. base anropar basklassens konstruktor, this anropar den andra konstruktorn i klassen
         public FuelCar() : this("DefaultBrandName", 100) { }
         public FuelCar(string brand, double fuelCapacity, string regNo = "AAA111") : base(regNo, fuelCapacity, brand) { }
 
+        //Nyckelordet sealed låser metoden från att overriadas längre
         public sealed override string Drive(int distance)
         {
             var result = new StringBuilder();
@@ -105,6 +125,7 @@ namespace OOP
                 result.AppendLine("Negative distance is assumed to be 0");
             }
 
+            //Samma resultat på rad 129, 130 bara olika syntax
             //FuelLevel = FuelLevel - (distance * fuelConsumption);
             FuelLevel -= distance * fuelConsumption;
 
@@ -116,6 +137,7 @@ namespace OOP
 
     }
 
+    //Klassen Bicycle kan inte ärvas när den är markerad med sealed
     public sealed class Bicycle : AbstractVehicle
     {
         public override string Turn()
@@ -128,6 +150,7 @@ namespace OOP
             return string.Format("Bicycle starts pedaling for {0} {1}",  distance, Turn());
         }
 
+        //Överladdade metoder måste ha unika signaturer. Olika namn samt parametrar. Returtyp inte del av metodens signatur
         public void Over(string input)
         {
 
