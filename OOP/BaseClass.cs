120f216664bbda2570552097e6a6b96d4c645d90
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2022-05-17 15:58:51 +0200

    TestCommit

---
+++ OOP/BaseClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+
+
+    internal abstract class BaseClass
+    {
+        public static void Do()
+        {
+            Console.WriteLine("Base do");
+        }
+
+    }
+
+    internal class DirevedClass : BaseClass
+    {
+        public  void Test()
+        {
+            Do();
+        }
+    }
+   
+}
+
